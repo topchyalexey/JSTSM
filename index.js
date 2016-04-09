3256afaab2ed28c3964840a0e14d3979bc71ab8d
akabab <ycribier@student.42.fr>
2016-04-09 15:23:12 +0200

    handling directory or file as source

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ var parsePath = require('parse-filepath');
 var nunjucks = require('nunjucks');
 nunjucks.configure({ autoescape: false });
 
+var fs = require('fs');
+
 // var jsonschema = require('jsonschema');
+// var draft4schema = JSON.parse(fs.readFileSync("./draft_schemas/draft-04/schema.json", 'utf8'));
+// console.log("draft4schema:", draft4schema);
+// var v = jsonschema.validate(json, draft4schema);
+// var v = jsonschema.validate({"one": 1}, draft4schema);
+// console.log("v:", v);
 
 var dateformat = require('dateformat');
 
@@ -22,68 +29,117 @@ var argv = require('yargs')
     .alias('p', 'project')
     .alias('a', 'author')
     .alias('c', 'company')
+    .boolean('u')
     .alias('u', 'use-struct') // default is class
+    .count('verbose')
+    .alias('v', 'verbose')
     .argv;
 
-var srcFilePath = argv.s;
-// console.log("src:", srcFilePath);
+VERBOSE_LEVEL = argv.verbose;
 
-// dir.readFiles(__dirname, {
-//     match: /.txt$/,
-//     exclude: /^\./
-//     }, function(err, content, next) {
-//         if (err) throw err;
-//         console.log('content:', content);
-//         next();
-//     },
-//     function(err, files){
-//         if (err) throw err;
-//         console.log('finished reading files:',files);
-//     });
+function WARN()  { VERBOSE_LEVEL >= 0 && console.log.apply(console, arguments); }
+function INFO()  { VERBOSE_LEVEL >= 1 && console.log.apply(console, arguments); }
+function DEBUG() { VERBOSE_LEVEL >= 2 && console.log.apply(console, arguments); }
 
 
-// READ
-var fs = require('fs');
-var jsonFile = fs.readFileSync(srcFilePath, 'utf8');
+// START
 
-var srcFileName = parsePath(srcFilePath).name;
-// console.log("srcFileName:", srcFileName);
+var srcPath = argv.s;
+try {
+    fs.accessSync(srcPath);
+    var stat = fs.lstatSync(srcPath);
 
-// PARSE & VALIDATE
-var json = JSON.parse(jsonFile);
-// console.log("json:", json);
+    if (stat.isDirectory()) {
+        dir.readFiles(srcPath, {
+                match: /.json$/,
+            }, function (err, fileContent, filePath, next) {
+                if (err) throw err;
 
-// var draft4schema = JSON.parse(fs.readFileSync("./draft_schemas/draft-04/schema.json", 'utf8'));
-// console.log("draft4schema:", draft4schema);
+                parseFile(filePath, fileContent);
+                next();
+            },
+            function (err, files){
+                if (err) throw err;
+            });
 
-// var v = jsonschema.validate(json, draft4schema);
-// var v = jsonschema.validate({"one": 1}, draft4schema);
-// console.log("v:", v);
+    }
+    else if (stat.isFile()) {
+        var fileContent = fs.readFileSync(srcPath, 'utf8');
 
-var properties = [];
-if (json.properties) {
-    properties = Object.keys(json.properties).map(key => {
-        var p = json.properties[key];
+        parseFile(srcPath, fileContent);
+    }
 
-        return {
-            key: key,
-            isRef: p.hasOwnProperty("$ref"),
-            type: typeForProperty(p),
-            required: p.required == true || json.required.contains(key)
-        };
-    });
+} catch (e) {
+    return WARN(e.message);
 }
+
+// METHODS
+
+function parseFile(filePath, fileContent) {
+    INFO("> Parsing", filePath);
+    var fileName = parsePath(filePath).name;
+
+    try {
+        var json = JSON.parse(fileContent);
+    } catch (e) {
+        return WARN("Parsing error:", e);
+    }
+
+    // TODO: Validate
+
+    // Prepare
+
+    var properties = [];
+    if (json.properties) {
+        properties = Object.keys(json.properties).map(key => {
+            var p = json.properties[key];
+
+            return {
+                key: key,
+                isRef: p.hasOwnProperty("$ref"),
+                type: typeForProperty(p),
+                required: p.required == true || json.required.contains(key)
+            };
+        });
+    }
+
+    // Render
+
+    var now = new Date();
+    var modelName = _.upperFirst(fileName);
+
+    var output = nunjucks.render('templates/template.swift', {
+        modelName: modelName,
+        projectName: argv.project || "<PROJECT_NAME>",
+        author: argv.author || "<AUTHOR>",
+        now: dateformat(now, "dd/mm/yy"),
+        copyright: now.getFullYear() + " " + (argv.company || "<COMPANY>"), // eg. Copyright © 2016 OpenJet
+        classOrStruct: argv['use-struct'] ? "struct" : "class",
+        properties: properties
+    });
+
+    // Write
+
+    mkdirp('output');
+
+    var destFilePath = "./output/" + (argv.o || modelName + ".swift");
+
+    fs.writeFile(destFilePath, output, function (err) {
+        if (err) { return WARN(err); }
+
+        INFO("File saved to:", destFilePath);
+    });
+
+}
+
+// HELPERS
 
 function typeForProperty(p) {
     var _basicTypes = {
         "string": "String",
         "integer": "Int",
-        "number": "Double", // or "Float" ?
+        "number": "Double",
         "boolean": "Bool",
-        // "array": "",
-        // "object": "",
-        // "null": "",
-        // any ?
     }
 
     if (p.hasOwnProperty("$ref")) {
@@ -103,38 +159,10 @@ function typeForProperty(p) {
                     case "boolean":
                         return _basicTypes[p.type];
                         break;
-                    default: console.warn("type not handled:", p.type); break;
+                    default: DEBUG("type not handled:", p.type); break;
                 }
                 break;
-            default: console.warn("typeof not handled:", typeof p.type); break;
+            default: DEBUG("typeof not handled:", typeof p.type); break;
         }
     }
-
 }
-
-// RENDER
-var now = new Date();
-var modelName = _.upperFirst(srcFileName);
-
-var output = nunjucks.render('templates/template.swift', {
-    modelName: modelName,
-    projectName: argv.project || "<PROJECT_NAME>",
-    author: argv.author || "<AUTHOR>",
-    now: dateformat(now, "dd/mm/yy"),
-    copyright: now.getFullYear() + " " + (argv.company || "<COMPANY>"), // eg. Copyright © 2016 OpenJet
-    classOrStruct: argv['use-struct'] ? "struct" : "class",
-    properties: properties
-});
-
-console.log(output);
-
-// WRITE
-mkdirp('output');
-
-var destFilePath = "./output/" + (argv.o || modelName + ".swift");
-
-fs.writeFile(destFilePath, output, function (err) {
-    if (err) { return console.log(err); }
-
-    console.log("File saved to:", destFilePath);
-});
